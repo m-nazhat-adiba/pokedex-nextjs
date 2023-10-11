5fea718a5b01829f68ae7c3101dc6930abfec08e
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-11 11:46:30 +0700

    added header section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,28 @@
-import NavBar from "@/components/navigation";
+import Button from "@/components/button/Base";
+import Layout from "@/components/layout";
+import Image from "next/image";
 import React from "react";
 
 function Home() {
   return (
-    <div className="container mx-auto">
-      <NavBar />
+    <div>
+      <Layout>
+        <div className="container mx-auto my-10">
+          <header className="flex justify-between">
+            <div className="flex flex-col gap-12 w-[40%] justify-center">
+              <h1 className="text-6xl font-semibold">
+                Find all your favorite Pokemon
+              </h1>
+              <h2 className="text-2xl">
+                Learn all the types of Pokemon, their strengths, weaknesses and
+                abilities
+              </h2>
+              <Button type="btn">See Pokemon</Button>
+            </div>
+            <Image src="/header-bg.png" width={792} height={680} alt="bg" />
+          </header>
+        </div>
+      </Layout>
     </div>
   );
 }
