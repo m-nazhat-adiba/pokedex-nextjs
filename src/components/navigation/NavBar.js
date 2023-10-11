92bae3bba15278f709eeeecaee7dc3e7d63d113d
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-11 11:45:35 +0700

    edited responsiveness

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -3,11 +3,11 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="flex justify-between w-full">
-      <Image src="/logo.png" width={152} height={90} alt="pokemon-logo" />
-      <ul className="flex gap-2">
-        <li>Home</li>
-        <li>About</li>
+    <nav className="flex justify-between w-full py-4 container mx-auto">
+      <Image src="/logo.png" width={120} height={60} alt="pokemon-logo" />
+      <ul className="flex gap-4 text-lg font-bold">
+        <li className="self-center">Home</li>
+        <li className="self-center">About</li>
       </ul>
     </nav>
   );
