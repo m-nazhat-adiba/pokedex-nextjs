ec3d39c368967a2a4a55d0017b71c1d5ba03bbcf
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-10-12 11:48:51 +0700

    edited button shadow

--- src/components/button/Base.js
+++ src/components/button/Base.js
@@ -8,7 +8,7 @@ const Button = ({ children, type }) => {
   return (
     <button
       type={`${TYPES[type]}`}
-      className="w-[231px] h-[66px] left-0 top-0 bg-green-400 rounded-[11px] shadow-[0px_-6px_0px_0px_rgba(0,0,0,0.18)_inset] text-center text-neutral-800 text-2xl font-bold"
+      className="w-[231px] h-[66px] left-0 top-0 bg-gradient-to-l from-green-400 to-green-500 rounded-[11px] shadow-lg text-center text-white text-2xl font-bold"
     >
       {children}
     </button>
